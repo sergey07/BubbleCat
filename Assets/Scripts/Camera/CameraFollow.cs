9418291b00b42b04dcb2986b52b8577a87df3116
brown85858585 <151971750+brown85858585@users.noreply.github.com>
2025-01-25 23:43:48 +0300

    zoomCamera toggle separated

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private float cameraOffset = -10.0f;
+
+    void LateUpdate()
+    {
+        if (targetObject != null)
+        {
+            transform.position = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y, targetObject.transform.position.z + cameraOffset);
+        }
+    }
+}
--- Assets/Scripts/Camera/CameraZoom.cs
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraZoom : MonoBehaviour
 {
-    [SerializeField] private GameObject targetObject;
-    [SerializeField] private float cameraOffset = -10.0f;
     public Camera cam;
     private bool isZoomBig = true;
-
     // interface
     public void ZoomCamerToggle() {
         if (isZoomBig) {
@@ -21,13 +18,7 @@ public class CameraFollow : MonoBehaviour
     {
         cam = GetComponent<Camera>();
     }
-    void LateUpdate()
-    {
-        if (targetObject != null)
-        {
-            transform.position = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y, targetObject.transform.position.z + cameraOffset);
-        }
-    }
+
     private void ZoomMakeSmall() {
         // Set the size of the viewing volume you'd like the orthographic Camera to pick up
         cam.orthographicSize = 6.0f;
