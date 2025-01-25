3e0e5050963f1bc290a69931104ae6c485a4ba9b
Sergey <kondor07@mail.ru>
2025-01-25 13:48:59 +0300

    Шарик надувается и сдувается

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float originSize = 1f;
+    [SerializeField] private float minSize = 1f;
+    [SerializeField] private float maxSize = 6f;
+    [SerializeField] private float scaleSpeed = 1f;
 
     private Rigidbody2D rb;
 
@@ -21,6 +26,26 @@ public class Player : MonoBehaviour
     private void HandleInput()
     {
         Vector2 inputVector = GameInput.Instance.GetMovementVector();
-        rb.MovePosition(rb.position + inputVector * (speed * Time.fixedDeltaTime));
+
+        if (inputVector.y > 0)
+        {
+            float scaleValue = ChangeScale(scaleSpeed);
+            transform.localScale = new Vector3(scaleValue, scaleValue, transform.localScale.y);
+        }
+        else if (inputVector.y < 0)
+        {
+            float scaleValue = ChangeScale(-scaleSpeed);
+            transform.localScale = new Vector3(scaleValue, scaleValue, transform.localScale.y);
+        }
+        //b.MovePosition(rb.position + inputVector * (speed * Time.fixedDeltaTime));
+    }
+
+    private float ChangeScale(float scaleSpeed)
+    {
+        float newScaleX = transform.localScale.x + scaleSpeed * Time.fixedDeltaTime;
+
+        //float newScaleX = transform.localScale.x + transform.localScale.x * scaleSpeed * Time.fixedDeltaTime;
+
+        return newScaleX;
     }
 }
