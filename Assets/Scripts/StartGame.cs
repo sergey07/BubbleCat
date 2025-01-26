f6bbce3f9368f7cf9ef95f43ef6524b96c47409c
Sergey <kondor07@mail.ru>
2025-01-26 11:35:39 +0300

    fix animation

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,10 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    [SerializeField] private float timeBeforeTranslateCatByWitch = 2.0f;
+
+    [SerializeField] private GameObject witchObject;
+    [SerializeField] private GameObject catObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(WaitForLoadFirstLevel());
+        //StartCoroutine(WaitForLoadFirstLevel());
+
+    }
+
+    IEnumerator TranslateCatByWitch()
+    {
+        yield return new WaitForSeconds(timeBeforeTranslateCatByWitch);
+
+        witchObject.transform.localScale = new Vector3(-witchObject.transform.localScale.x, witchObject.transform.localScale.y, witchObject.transform.localScale.z);
     }
 
     IEnumerator WaitForLoadFirstLevel()
