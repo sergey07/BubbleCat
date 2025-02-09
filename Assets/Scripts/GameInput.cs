679cb4ec0b02fa366dc59460d99cdc18041d9291
Sergey <kondor07@mail.ru>
2025-02-09 12:42:21 +0300

    Clean prefabs, convert WASD movement to old Input System

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -7,7 +7,8 @@ public class GameInput : MonoBehaviour
 
     [SerializeField] private FixedJoystick _fixedJoystick;
 
-    private PlayerInputActions _playerInputActions;
+    //private PlayerInputActions _playerInputActions;
+
     private bool _isJoystickVisible = false;
 
     private void Awake()
@@ -19,8 +20,8 @@ public class GameInput : MonoBehaviour
 
             Instance = this;
 
-            _playerInputActions = new PlayerInputActions();
-            _playerInputActions.Enable();
+            //_playerInputActions = new PlayerInputActions();
+            //_playerInputActions.Enable();
 
             ShowJoystick();
         }
@@ -43,10 +44,27 @@ public class GameInput : MonoBehaviour
 
     public Vector2 GetMovementVector()
     {
-        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+        //Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2
 
-        if (_playerInputActions.Player.Move.ReadValue<Vector2>() != Vector2.zero &&
-        _playerInputActions.Player.Move.ReadValue<Vector2>() != null)
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            inputVector.y = 1;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            inputVector.y = -1;
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            inputVector.x = -1;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            inputVector.x = 1;
+        }
+
+        if (inputVector != null && inputVector != Vector2.zero)
         {
             // print(_playerInputActions.Player.Move.ReadValue<Vector2>());
             HideJoystick();
@@ -57,12 +75,10 @@ public class GameInput : MonoBehaviour
         // Если схватил за джойстик, то он перезапишет значения - OnlyMeRus
         if (_fixedJoystick != null && (_fixedJoystick.Vertical != 0 || _fixedJoystick.Horizontal != 0))
         {
-            inputVector = new Vector2(
-                _fixedJoystick.Horizontal,
-                _fixedJoystick.Vertical
-            );
-            inputVector.Normalize();
+            inputVector = new Vector2(_fixedJoystick.Horizontal, _fixedJoystick.Vertical);
         }
+
+        inputVector.Normalize();
 
         return inputVector;
     }
